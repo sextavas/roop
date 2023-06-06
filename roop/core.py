289a8ab8a177b222b0e9dcfa996a4072fcae8b8f
hassan-sd <119671806+hassan-sd@users.noreply.github.com>
2023-06-06 15:20:04 +0100

    remove nsfw

--- roop/core.py
+++ roop/core.py
@@ -15,7 +15,6 @@ import torch
 import tensorflow
 from pathlib import Path
 import multiprocessing as mp
-from opennsfw2 import predict_video_frames, predict_image
 import cv2
 
 import roop.globals
@@ -175,15 +174,6 @@ def start(preview_callback = None):
     if not test_face:
         print("\n[WARNING] No face detected in source image. Please try with another one.\n")
         return
-    if is_img(target_path):
-        if predict_image(target_path) > 0.85:
-            quit()
-        process_img(args.source_img, target_path, args.output_file)
-        status("swap successful!")
-        return
-    seconds, probabilities = predict_video_frames(video_path=args.target_path, frame_interval=100)
-    if any(probability > 0.85 for probability in probabilities):
-        quit()
     video_name_full = target_path.split("/")[-1]
     video_name = os.path.splitext(video_name_full)[0]
     output_dir = os.path.dirname(target_path) + "/" + video_name if os.path.dirname(target_path) else video_name
